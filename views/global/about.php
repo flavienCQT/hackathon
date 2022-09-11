fbe74f8f1a67e63f67f7a16ceb54513c111c7598
flavienCQT <flavien.coquetpro@gmail.com>
2022-09-11 19:43:44 +0200

    Début de complétion de la page "A propos"

--- views/global/about.php
+++ views/global/about.php
@@ -1,3 +1,7 @@
 <div class="d-flex justify-content-center vh-100 align-items-center">
-    <h1>ABOUT</h1>
+    <h1>A propos</h1>
 </div>
+<div class="d-flex justify-content-center vh-100 align-items-center">
+    <p>Bienvenue sur la page "A propos" de cette hackathon</p>
+</div>
+
