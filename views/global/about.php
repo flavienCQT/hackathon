0522f25c4f80a996da81f049026ee15ae558766d
flavienCQT <flavien.coquetpro@gmail.com>
2022-09-18 18:53:47 +0200

    Add CGU on "About" page !

--- views/global/about.php
+++ views/global/about.php
@@ -2,6 +2,7 @@
     <h1>A propos</h1>
 </div>
 <div class="d-flex justify-content-center vh-100 align-items-center">
-    <p>Bienvenue sur la page "A propos" de cette hackathon<, ici nous vous proposons un hackathon/p>
+    <p>Bienvenue sur la page "A propos" de cette hackathon<, ici nous vous proposons un hackathon</p>
+    <a href="public/img/CGU.pdf">CGU</a>
 </div>
 
